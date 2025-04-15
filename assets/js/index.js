d2c2ffe12d397afeabcc7b2eaef385385ff3b2a8
NitinSangwan-584 <nitinsangwan363@gmail.com>
2025-04-15 16:23:59 +0530

    processing

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+//----------------------------------------------back to top------------------------------------------------
+const myButton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+    myButton.style.display = window.scrollY > 200 ? "block" : "none";
+};
+
+function topFunction() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
